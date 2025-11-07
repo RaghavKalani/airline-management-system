02f858174c086602cb9aef65c3a5bb804c09d98f
RaghavKalani <raghavkalani2225@gmail.com>
2025-11-07 09:01:47 +0530

    Update SQL script to add world cities and flights

--- database/add_world_cities_simple.sql
+++ database/add_world_cities_simple.sql
@@ -1,6 +1,3 @@
--- Add 50+ major world cities - SIMPLE VERSION
-
--- Step 1: Add airports/cities
 INSERT IGNORE INTO airports (airport_code, airport_name, city, country) VALUES
 ('BKK', 'Suvarnabhumi Airport', 'Bangkok', 'Thailand'),
 ('SIN', 'Singapore Changi Airport', 'Singapore', 'Singapore'),
@@ -27,8 +24,7 @@ INSERT IGNORE INTO airports (airport_code, airport_name, city, country) VALUES
 ('YYZ', 'Toronto Airport', 'Toronto', 'Canada'),
 ('GRU', 'Guarulhos Airport', 'Sao Paulo', 'Brazil'),
 ('JNB', 'O.R. Tambo Airport', 'Johannesburg', 'South Africa');
-
--- Step 2: Add more airlines  
+ 
 INSERT IGNORE INTO airlines (airline_code, airline_name, country) VALUES
 ('SQ', 'Singapore Airlines', 'Singapore'),
 ('CX', 'Cathay Pacific', 'Hong Kong'),
@@ -42,7 +38,6 @@ INSERT IGNORE INTO airlines (airline_code, airline_name, country) VALUES
 ('UA', 'United Airlines', 'USA'),
 ('QF', 'Qantas', 'Australia');
 
--- Step 3: Get airline and airport IDs for reference
 SET @AI_id = (SELECT airline_id FROM airlines WHERE airline_code = 'AI' LIMIT 1);
 SET @6E_id = (SELECT airline_id FROM airlines WHERE airline_code = '6E' LIMIT 1);
 SET @SQ_id = (SELECT airline_id FROM airlines WHERE airline_code = 'SQ' LIMIT 1);
@@ -70,8 +65,6 @@ SET @JFK = (SELECT airport_id FROM airports WHERE airport_code = 'JFK' LIMIT 1);
 SET @LAX = (SELECT airport_id FROM airports WHERE airport_code = 'LAX' LIMIT 1);
 SET @SYD = (SELECT airport_id FROM airports WHERE airport_code = 'SYD' LIMIT 1);
 
--- Step 4: Add flights
--- India to Asia
 INSERT INTO flights (flight_number, airline_id, origin_airport_id, destination_airport_id, total_seats, base_price) VALUES
 ('AI301', @AI_id, @DEL, @SIN, 180, 25000.00),
 ('AI302', @AI_id, @DEL, @BKK, 180, 22000.00),
@@ -81,31 +74,26 @@ INSERT INTO flights (flight_number, airline_id, origin_airport_id, destination_a
 ('6E401', @6E_id, @DEL, (SELECT airport_id FROM airports WHERE airport_code = 'KUL' LIMIT 1), 150, 20000.00),
 ('6E402', @6E_id, @BLR, @SIN, 150, 19000.00),
 
--- India to Middle East
 ('AI601', @AI_id, @DEL, @DOH, 180, 18000.00),
 ('AI602', @AI_id, @BOM, @AUH, 180, 17000.00),
 ('QR101', @QR_id, @DEL, @DOH, 200, 19000.00),
 ('QR102', @QR_id, @BOM, @DOH, 200, 18500.00),
 ('EY201', @EY_id, @DEL, @AUH, 200, 17500.00),
 
--- India to Europe
 ('AI121', @AI_id, @DEL, @CDG, 250, 45000.00),
 ('AI122', @AI_id, @DEL, @FRA, 250, 44000.00),
 ('BA903', @BA_id, @BOM, @LHR, 250, 48000.00),
 ('LH501', @LH_id, @DEL, @FRA, 250, 46000.00),
 ('AF601', @AF_id, @DEL, @CDG, 250, 47000.00),
 
--- India to USA
 ('AI191', @AI_id, @DEL, @JFK, 300, 75000.00),
 ('AI192', @AI_id, @BOM, @JFK, 300, 76000.00),
 ('UA101', @UA_id, @DEL, (SELECT airport_id FROM airports WHERE airport_code = 'SFO' LIMIT 1), 300, 78000.00),
 ('AA201', @AA_id, @DEL, @LAX, 300, 77000.00),
 
--- India to Australia
 ('AI401', @AI_id, @DEL, @SYD, 280, 65000.00),
 ('AI402', (SELECT airline_id FROM airlines WHERE airline_code = 'QF' LIMIT 1), @BOM, @SYD, 280, 64000.00);
 
--- Step 5: Add schedules for new flights (next 7 days)
 INSERT INTO schedules (flight_id, departure_time, arrival_time, available_seats, status)
 SELECT 
     flight_id,
@@ -133,5 +121,4 @@ SELECT
     'SCHEDULED'
 FROM flights WHERE flight_number IN ('AI191', 'AI192', 'UA101', 'AA201', 'AI401');
 
--- Done! You now have 50+ cities and 25+ new flights
 SELECT 'SUCCESS: Added 25+ new cities and flights!' as status;
